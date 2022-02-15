ca2ab2702265565b8b86b79a42af8013c8bd9e26
dawid <dawidp789@gmail.com>
2022-02-15 15:02:46 +0100

    implement jwt utils

---
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/utils/JwtResponse.java
@@ -0,0 +1,25 @@
+package com.pentagon.cafe.virtualSmallJobFinder.utils;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class JwtResponse {
+
+    private String accessToken;
+    private Long id;
+    private String username;
+    private String email;
+    private Set<String> roles;
+    private final String type = "Bearer";
+
+    public JwtResponse(String accessToken, Long id, String username, String email, Set<String> roles) {
+        this.accessToken = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+}
