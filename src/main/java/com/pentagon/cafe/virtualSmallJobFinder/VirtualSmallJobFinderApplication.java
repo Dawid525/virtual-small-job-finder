5b9c8e1141965dc719c6b217709338748131a9b6
dawid <dawidp789@gmail.com>
2022-02-15 14:37:53 +0100

    init project

---
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/VirtualSmallJobFinderApplication.java
@@ -0,0 +1,13 @@
+package com.pentagon.cafe.virtualSmallJobFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualSmallJobFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualSmallJobFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pentagon/cafe/virtualSmallJobFinder/VirtualSmallJobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentagon.cafe.virtualSmallJobFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualSmallJobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
