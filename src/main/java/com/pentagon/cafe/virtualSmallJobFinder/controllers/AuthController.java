79c821bdbac674b6a03d15cee074c2df67d25d51
dawid <dawidp789@gmail.com>
2022-02-15 15:19:35 +0100

    refactor auth controller, methods return ResponseEntity

--- src/main/java/com/pentagon/cafe/virtualSmallJobFinder/controllers/AuthController.java
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/controllers/AuthController.java
@@ -6,6 +6,7 @@ import com.pentagon.cafe.virtualSmallJobFinder.utils.JwtResponse;
 import com.pentagon.cafe.virtualSmallJobFinder.utils.LoginRequest;
 import com.pentagon.cafe.virtualSmallJobFinder.utils.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +21,13 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public JwtResponse loginUser(@RequestBody LoginRequest loginRequest){
-        return authenticationService.authenticateLoginRequest(loginRequest);
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(authenticationService.authenticateLoginRequest(loginRequest));
     }
 
     @PostMapping("/register")
-    public Long registerUser(@RequestBody RegisterRequest registerRequest){
-        return userService.addUser(registerRequest);
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest){
+        return ResponseEntity.status(201).body("User registered successfully with id:" + userService.addUser(registerRequest));
     }
 
 }
