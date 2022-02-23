7995787ca9b087c32674bb97e75c375123af7722
dawid <dawidp789@gmail.com>
2022-02-23 23:44:48 +0100

    add response body in handlers

--- src/main/java/com/pentagon/cafe/virtualSmallJobFinder/controllers/handlers/Handler.java
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/controllers/handlers/Handler.java
@@ -9,6 +9,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
@@ -18,6 +19,7 @@ import java.util.Map;
 @ControllerAdvice
 public class Handler {
 
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
@@ -29,19 +31,21 @@ public class Handler {
         });
         return errors;
     }
-
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(TokenRefreshException.class)
     public ErrorDto handleTokenRefreshException(TokenRefreshException ex) {
         return new ErrorDto(ex.getMessage(), "11");
     }
 
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserWithThisEmailExistsException.class)
     public ErrorDto handleUserWithThisEmailExistsException(UserWithThisEmailExistsException ex) {
         return new ErrorDto(ex.getMessage(), "12");
     }
 
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserWithThisUsernameExistsException.class)
     public ErrorDto handleUserWithThisUsernameExistsException(UserWithThisUsernameExistsException ex) {
