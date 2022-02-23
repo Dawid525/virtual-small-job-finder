60a787c04419852e316f559aacd504c4a931cc9b
dawid <dawidp789@gmail.com>
2022-02-24 00:34:42 +0100

    refactor: change validation annotations

--- src/main/java/com/pentagon/cafe/virtualSmallJobFinder/payload/LoginRequest.java
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/payload/LoginRequest.java
@@ -10,9 +10,11 @@ import javax.validation.constraints.NotNull;
 public class LoginRequest {
 
     @NotNull(message = "Username is mandatory")
-    @NotBlank(message = "Username is mandatory")
+    @NotBlank
     @Length(min = 5, max = 25, message = "Username must have 5-25 characters")
     private String username;
+    @NotNull(message = "Password is mandatory")
+    @NotBlank
     @ValidPassword
     private String password;
 }
--- src/main/java/com/pentagon/cafe/virtualSmallJobFinder/payload/PasswordRequest.java
+++ src/main/java/com/pentagon/cafe/virtualSmallJobFinder/payload/PasswordRequest.java
@@ -3,8 +3,13 @@ package com.pentagon.cafe.virtualSmallJobFinder.payload;
 import com.pentagon.cafe.virtualSmallJobFinder.validation.ValidPassword;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class PasswordRequest {
+    @NotNull(message = "Password is mandatory")
+    @NotBlank
     @ValidPassword
     private String password;
 }
